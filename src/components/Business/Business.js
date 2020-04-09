404d981573de498b1aa14a58744d9fcb1c71fa3e
Lillis <lidiane.snobre@gmail.com>
2020-04-09 16:22:40 -0300

    Added new info until lesson n34 from CodeCademy

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,31 +1,5 @@
 import React from 'react';
 import './Business.css';
-import ReactDom from "react-dom";
-
-class Business extends React.Component {
-    render() {
-        return (
-            <div>
-                <div className="image-container">
-                    <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
-                </div>
-                <h2>MarginOtto Pizzeria</h2>
-                <div className="Business-Information">
-                    <div class="Business-address">
-                        <p>1010 Paddington Way</p>
-                        <p>Bordertown</p>
-                        <p>NY 10101</p>
-                    </div>
-                    <div class="Business-reviews">
-                        <h3>ITALIAN</h3>
-                        <h3 class="rating">4.5 stars</h3>
-                        <p>90 reviews</p>
-                    </div>     
-                </div>
-            </div>
-        )      
-    }
-}
 
 const business = {
     imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
@@ -37,6 +11,32 @@ const business = {
     category: 'Italian',
     rating: 4.5,
     reviewCount: 90
+};
+
+class Business extends React.Component {
+    render() {
+        return (
+            <div className="Business">
+                <div className="image-container">
+                    <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
+                </div>
+                <h2>{business.name}</h2>
+                <div className="Business-Information">
+                    <div className="Business-address">
+                        <p>{business.address}</p>
+                        <p>{business.city}</p>
+                        <p>{business.state}{business.zipCode}</p>
+                    </div>
+                    <div className="Business-reviews">
+                        <h3>{business.category}</h3>
+                        <h3 className="rating">{business.rating} stars</h3>
+                        <p>{business.reviewCount} reviews</p>
+                    </div>     
+                </div>
+            </div>
+        )      
+    }
 }
- 
+
+export default Business;
     
