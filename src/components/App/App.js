10e8bef0cbeb46760dc03e8b597a93b63eadf77c
Lillis <lidiane.snobre@gmail.com>
2020-04-10 11:05:06 -0300

    Add the first part

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import logo from '/Users/lidiane/Projetos/ravenous/src/logo.svg';
 import './App.css';
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>ravenous</h1>
+          <SearchBar />
+          <BusinessList />
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
+
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -8,7 +8,33 @@ const sortByOptions = {
 }
 
 class SearchBar extends React.Component {
+    //Creating a method:
     renderSortByOptions() {
-        return 
+        /* Iterate throught the object: First access the keys of the sortByOptions, then call method pass in "sortByOptions" as the argument. Iterate the using the map() method.Then pass a callback function to the map() as an arg, the parameter should called "sortByOption" */
+        return Object.keys(sortByOptions).map(sortByOption => {
+            //Let's store the object values:
+            let sortByOptionValue = sortByOptions[sortByOption];
+            return <li key={sortByOptionValue}>{sortByOption}</li>
+        });
+    }
+    render() {
+        return (
+            <div className="SearchBar">
+                <div className="SearchBar-sort-options">
+                    <ul>
+                        {this.renderSortByOptions()}
+                    </ul>
+                </div>
+                <div className="SearchBar-fields">
+                    <input placeholder="Search Businesses" />
+                    <input placeholder="Where?" />
+                </div>
+                <div className="SearchBar-submit">
+                    <a href>Let's Go</a>
+                </div>
+            </div>
+        )
     }
 }
+
+export default SearchBar;
